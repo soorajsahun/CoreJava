0ac8e63f9d751a463b1080d1ee9160d9750afc93
SurajSahuGitHub <79203537+soorajsahun@users.noreply.github.com>
2021-07-22 16:01:56 +0530

    Update CollectionDemo6.java

--- basic/src/hit/day28/CollectionDemo6.java
+++ basic/src/hit/day28/CollectionDemo6.java
@@ -15,15 +15,19 @@ public class CollectionDemo6 {
 		prop.put("3", "Manoj");
 		prop.put("4", "Rohit");
 		
-		//first reading method
+		//first reading method//we provide key it retirns value
 		System.out.println(prop.get("2"));
 		
-		//Second reading method
+		//Second reading method//this returns value
 		Enumeration e=prop.elements();
 		while(e.hasMoreElements()) {
+//			String value=(String)e.nextElement();
+//			System.out.println(value);
+//			or
 			System.out.println(e.nextElement());
 		}
 		
+		//Third reading method//this returns key,value pair
 		Set set=prop.entrySet();//Memorize syntax
 		Iterator iter=set.iterator();
 		while(iter.hasNext()) {
